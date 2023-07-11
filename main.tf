76b2b8f58c5d66e2f4a9569966e45a8f2bdd2a3e
Asad Shahid <62490374+AsadShahid04@users.noreply.github.com>
2023-07-11 00:02:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,23 @@
+terraform {
+  required_providers {
+    aviatrix = {
+      source = "AviatrixSystems/aviatrix"
+      version = "~>2.22.0"
+    }
+  }
+
+# Create an AWS VPC
+resource "aviatrix_vpc" "aws_vpc" {
+  cloud_type           = 1
+  account_name         = "SelfService-AWS"
+  region               = "us-west-1"
+  name                 = "aws-vpc"
+  cidr                 = "10.0.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
+
+
 /*
 
 // ACE-IAC Core Aviatrix Infrastructure
@@ -86,23 +106,3 @@ resource "aviatrix_segmentation_network_domain" "BU2" {
   domain_name_2 = "BU2"
   depends_on    = [aviatrix_segmentation_network_domain.BU1, aviatrix_segmentation_network_domain.BU2]
 } */
-
-
-terraform {
-  required_providers {
-    aviatrix = {
-      source = "AviatrixSystems/aviatrix"
-      version = "~>2.22.0"
-    }
-  }
-
-# Create an AWS VPC
-resource "aviatrix_vpc" "aws_vpc" {
-  cloud_type           = 1
-  account_name         = "SelfService-AWS"
-  region               = "us-west-1"
-  name                 = "aws-vpc"
-  cidr                 = "10.0.0.0/16"
-  aviatrix_transit_vpc = false
-  aviatrix_firenet_vpc = false
-}
